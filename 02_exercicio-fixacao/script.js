ab6f1640b7b63b0441c23ee31b2594646e400a2e
LucasBreia <lucas.devfullstack021@gmail.com>
2022-10-19 21:05:18 -0300

    Exercícos concluídos

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,18 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const diferencaIdade = minhaIdade - idadeAmiga
 
-// EXERCICIO 02
+console.log("A sua idade é maior que a do seu amigo?", minhaIdade > idadeAmiga)
+console.log("A diferença de idade é:", diferencaIdade)
+
+//EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d 
+b && a && !d
+a || d
+c && !d
